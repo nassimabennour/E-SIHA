110137b87cf131b65f2f4e5d999209fc119f0b74
jout <yassinejout@gmail.com>
2022-09-22 00:14:45 +0200

    fix(web): add config for react native web

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const { alias, configPaths } = require('react-app-rewire-alias');
+
+module.exports = function override(config, env) {
+  return alias(configPaths('./tsconfig.paths.json'))(config);
+};
