df869e9e75b1f2c0702b62669e6df62b75c21ea8
nassimabennour <73443835+nassimabennour@users.noreply.github.com>
2022-09-24 23:14:19 +0100

    fix(Input): fix text size

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -17,16 +17,17 @@ export const Input: FC<Props> = ({
       placeholder={placeHolder}
       placeholderTextColor="gray"
       secureTextEntry={password}
-      style={[styles.container, { borderBottomWidth: borderBottom ? 1 : 0 }]}
+      style={[styles.textStyle, { borderBottomWidth: borderBottom ? 1 : 0 }]}
     />
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  textStyle: {
     height: 15,
     margin: 5,
     paddingVertical: 10,
     paddingHorizontal: 5,
+    fontSize: 10
   },
 });
