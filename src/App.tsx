35e79d845b1f02a2d3e9ba1c88cd80b349cc0c81
nassimabennour <73443835+nassimabennour@users.noreply.github.com>
2022-09-24 23:53:24 +0100

    feat(authentication): add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { Button } from "./components/atoms/Button";
-import { Input } from "./components/atoms/Input";
-import { Typography } from "./components/atoms/Typography";
-import { CardWrapper } from "./components/wrappers/CardWrapper";
+import { LoginView } from "./features/authentication/login";
 
 const App = () => {
   return (
     <SafeAreaView>
-      <CardWrapper>
-        <Typography text="Sign in" textType="H3" />
-        <Input placeHolder="username" borderBottom={true} password={false} />
-        <Input placeHolder="password" borderBottom={true} password={true} />
-        <Button
-          disabled={true}
-          stretched={false}
-          outlined={false}
-          type="primary"
-          onPress={() => {
-            console.log("pressed");
-          }}
-          text="Go"
-        />
-      </CardWrapper>
+      <LoginView />
     </SafeAreaView>
   );
 };
---
+++ src/features/authentication/login/fixture.ts
@@ -0,0 +1,17 @@
+export const content = {
+    title : "Sign in",
+    placeholders: {
+        username: "Username",
+        password: "Password"
+    },
+    buttonText: "Go"
+}
+
+export const contentFr = {
+    title : "Se connecter",
+    placeholders: {
+        username: "Nom d'utilisateur",
+        password: "Mot de passe"
+    },
+    buttonText: "Valider"
+}
